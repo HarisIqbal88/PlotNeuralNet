5a288bd0b383022861f15ade1e7eca92ac0b283a
Jonas Eichhorn <eichhorn.jonas@uni-jena.de>
2022-12-29 17:55:28 +0100

    still fixing

--- PlotNeuralNetPy/__init__.py
+++ PlotNeuralNetPy/__init__.py
@@ -2,4 +2,3 @@
 # -*- coding: utf-8 -*-
 from .pycore import *
 from .pyexamples import *
-#from .version import __version__
--- PlotNeuralNetPy/version.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__version__ = "1.0.0"
