9b6a5171f74837e76983bb8bdc9bc50d6cfd1e12
Jonas Eichhorn <eichhorn.jonas@uni-jena.de>
2022-12-29 17:20:16 +0100

    created installable python package

---
+++ pycore/version.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__version__ = "1.0.0"
---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    from setuptools import setup, find_packages
+    setup(packages=find_packages())
