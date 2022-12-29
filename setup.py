409aa010277f4af0a4111751957efab45b618d24
Jonas Eichhorn <eichhorn.jonas@uni-jena.de>
2022-12-29 18:05:12 +0100

    fixing

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@
 
 if __name__ == "__main__":
     from setuptools import setup, find_packages
-    setup(packages=["pycore", "pyexamples"])
+    setup(packages=["PlotNeuralNetPy", 
+                    "PlotNeuralNetPy.pycore", 
+                    "PlotNeuralNetPy.pyexamples"])
