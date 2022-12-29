f21e74baf55a7b0e0bb6fe7d0950e8b5f56ce58b
Jonas Eichhorn <eichhorn.jonas@uni-jena.de>
2022-12-29 17:43:40 +0100

    made paths relative

--- PlotNeuralNetPy/__init__.py
+++ PlotNeuralNetPy/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import pycore as core
-import pyexamples as examples
-from version import __version__
+import .pycore as core
+import .pyexamples as examples
+from .version import __version__
